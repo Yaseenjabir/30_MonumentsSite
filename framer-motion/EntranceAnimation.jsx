0afebeaf01d88323579796337819955f630d0f4d
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-09-11 15:04:56 +0500

    changes made

--- framer-motion/EntranceAnimation.jsx
+++ framer-motion/EntranceAnimation.jsx
@@ -5,7 +5,7 @@ export default function EntranceAnimation() {
       <motion.div
         style={{ zIndex: 20000, height: "100vh" }}
         initial={{ x: 0 }}
-        animate={{ x: 0 }}
+        animate={{ x: "100%" }}
         transition={{ duration: 1.5, delay: 2.7, ease: "easeInOut" }}
         className="w-full h-screen fixed top-0 left-0 bg-black flex flex-col items-center justify-center"
       >
